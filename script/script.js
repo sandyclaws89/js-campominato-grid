b13c10673d6da0ab35776ef9c87ac4d179e86562
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-02 16:53:08 +0100

    impostazione funzione button (non riesco a far tornare i conti con il css e calc)

--- script/script.js
+++ script/script.js
@@ -1,5 +1,29 @@
 const container = document.getElementById('output');
+let playButton = document.getElementById('button');
+let selectedElement = document.getElementById('select_difficult');
 
-for (i=1; i<=100; i++) {
-    container.innerHTML += `<div class=box>${i}</div>`
+playButton.addEventListener('click', play);
+
+// for (i=1; i<=100; i++) {
+//     container.innerHTML += `<div class=box>${i}</div>`
+// }
+function play(){
+    let selectDifficult = selectedElement.value;
+    if (selectDifficult == "easy"){
+        for (i=1; i<=49; i++) {
+           container.innerHTML += `<div class=box>${i}</div>`
+        }
+        console.log("ciao");
+    } else if(selectDifficult == "medium"){
+        for (i=1; i<=81; i++) {
+            container.innerHTML += `<div class=box>${i}</div>`
+        }
+        console.log("rete");
+    }
+    else if (selectDifficult == "hard"){
+        for (i=1; i<=100; i++) {
+            container.innerHTML += `<div class=box>${i}</div>`
+        }
+        console.log("cane");
+    }
 }
